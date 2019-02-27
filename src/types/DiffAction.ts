b7d8b893e9ce825dd82585834ff39fd24303713b
nonoroazoro <xiaochao.k@gmail.com>
2019-02-27 18:03:10 +0800

    Add DiffMatchPatch and DiffAction

---
+++ src/types/DiffAction.ts
@@ -0,0 +1,11 @@
+/**
+ * The data structure representing a diff is an array of tuples:
+ * [[DIFF_DELETE, 'Hello'], [DIFF_INSERT, 'Goodbye'], [DIFF_EQUAL, ' world.']]
+ * which means: delete 'Hello', add 'Goodbye' and keep ' world.'
+ */
+export enum DiffAction
+{
+    DIFF_DELETE = -1,
+    DIFF_INSERT = 1,
+    DIFF_EQUAL = 0
+}
