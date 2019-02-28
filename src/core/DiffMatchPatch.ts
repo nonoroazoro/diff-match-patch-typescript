232387e3104f0174243e452b942e885facf24150
nonoroazoro <xiaochao.k@gmail.com>
2019-02-28 16:56:33 +0800

    Add patch_apply

--- src/core/DiffMatchPatch.ts
+++ src/core/DiffMatchPatch.ts
@@ -1210,6 +1210,140 @@ export class DiffMatchPatch
         }
         return patchesCopy;
     }
+
+    /**
+     * Merge a set of patches onto the text.  Return a patched text, as well
+     * as a list of true/false values indicating which patches were applied.
+     *
+     * @param {PatchObject[]} patches Array of Patch objects.
+     * @param {string} text Old text.
+     * @returns {[string, boolean[]]} Two element Array, containing the
+     * new text and an array of boolean values.
+     */
+    public patch_apply(patches: PatchObject[], text: string): [string, boolean[]]
+    {
+        if (patches.length === 0)
+        {
+            return [text, []];
+        }
+
+        // Deep copy the patches so that no changes are made to originals.
+        patches = this.patch_deepCopy(patches);
+
+        const nullPadding = this.patch_addPadding(patches);
+        text = nullPadding + text + nullPadding;
+
+        this.patch_splitMax(patches);
+        // delta keeps track of the offset between the expected and actual location
+        // of the previous patch.  If there are patches expected at positions 10 and
+        // 20, but the first patch was found at 12, delta is 2 and the second patch
+        // has an effective expected position of 22.
+        let delta = 0;
+        const results: boolean[] = [];
+        for (let x = 0; x < patches.length; x++)
+        {
+            const expected_loc = patches[x].start2 + delta;
+            const text1 = this.diff_text1(patches[x].diffs);
+            let start_loc: number;
+            let end_loc = -1;
+            if (text1.length > this.Match_MaxBits)
+            {
+                // patch_splitMax will only provide an oversized pattern in the case of
+                // a monster delete.
+                start_loc = this.match_main(text, text1.substring(0, this.Match_MaxBits),
+                    expected_loc);
+                if (start_loc !== -1)
+                {
+                    end_loc = this.match_main(text,
+                        text1.substring(text1.length - this.Match_MaxBits),
+                        expected_loc + text1.length - this.Match_MaxBits);
+                    if (end_loc === -1 || start_loc >= end_loc)
+                    {
+                        // Can't find valid trailing context.  Drop this patch.
+                        start_loc = -1;
+                    }
+                }
+            }
+            else
+            {
+                start_loc = this.match_main(text, text1, expected_loc);
+            }
+            if (start_loc === -1)
+            {
+                // No match found.  :(
+                results[x] = false;
+                // Subtract the delta for this failed patch from subsequent patches.
+                delta -= patches[x].length2 - patches[x].length1;
+            }
+            else
+            {
+                // Found a match.  :)
+                results[x] = true;
+                delta = start_loc - expected_loc;
+                let text2;
+                if (end_loc === -1)
+                {
+                    text2 = text.substring(start_loc, start_loc + text1.length);
+                }
+                else
+                {
+                    text2 = text.substring(start_loc, end_loc + this.Match_MaxBits);
+                }
+                if (text1 === text2)
+                {
+                    // Perfect match, just shove the replacement text in.
+                    text = text.substring(0, start_loc)
+                        + this.diff_text2(patches[x].diffs)
+                        + text.substring(start_loc + text1.length);
+                }
+                else
+                {
+                    // Imperfect match.  Run a diff to get a framework of equivalent
+                    // indices.
+                    const diffs = this.diff_main(text1, text2, false);
+                    if (text1.length > this.Match_MaxBits &&
+                        this.diff_levenshtein(diffs) / text1.length > this.Patch_DeleteThreshold)
+                    {
+                        // The end points match, but the content is unacceptably bad.
+                        results[x] = false;
+                    }
+                    else
+                    {
+                        this.diff_cleanupSemanticLossless(diffs);
+                        let index1 = 0;
+                        let index2;
+                        for (let y = 0; y < patches[x].diffs.length; y++)
+                        {
+                            const mod = patches[x].diffs[y];
+                            if (mod[0] !== DiffOperation.DIFF_EQUAL)
+                            {
+                                index2 = this.diff_xIndex(diffs, index1);
+                            }
+                            if (mod[0] === DiffOperation.DIFF_INSERT)
+                            {  // Insertion
+                                text = text.substring(0, start_loc + index2) + mod[1]
+                                    + text.substring(start_loc + index2);
+                            }
+                            else if (mod[0] === DiffOperation.DIFF_DELETE)
+                            {  // Deletion
+                                text = text.substring(0, start_loc + index2)
+                                    + text.substring(start_loc
+                                        + this.diff_xIndex(diffs, index1 + mod[1].length)
+                                    );
+                            }
+                            if (mod[0] !== DiffOperation.DIFF_DELETE)
+                            {
+                                index1 += mod[1].length;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        // Strip the padding off.
+        text = text.substring(nullPadding.length, text.length - nullPadding.length);
+        return [text, results];
+    }
     //#endregion PATCH FUNCTIONS (public)
 
     //#region DIFF FUNCTIONS (private)
