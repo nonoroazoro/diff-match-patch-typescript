84b7d50622342f2bbf41260fdc23c1f750ec8ecd
nonoroazoro <xiaochao.k@gmail.com>
2019-02-28 11:15:13 +0800

    Add diff_linesToChars_ and diff_linesToCharsMunge_

--- src/DiffMatchPatch.ts
+++ src/DiffMatchPatch.ts
@@ -489,4 +489,93 @@ export class DiffMatchPatch
 
         return diffsA.concat(diffsB);
     }
+
+    /**
+     * Split two texts into an array of strings. Reduce the texts to a string of
+     * hashes where each Unicode character represents one line.
+     *
+     * @private
+     * @param {string} text1 First string.
+     * @param {string} text2 Second string.
+     * @returns {{chars1: string, chars2: string, lineArray: string[]}}
+     * An object containing the encoded text1, the encoded text2 and
+     * the array of unique strings.
+     * The zeroth element of the array of unique strings is intentionally blank.
+     */
+    private diff_linesToChars_(text1: string, text2: string): { chars1: string; chars2: string; lineArray: string[]; }
+    {
+        const lineArray: string[] = [];  // e.g. lineArray[4] == 'Hello\n'
+        const lineHash: Record<string, number> = {};   // e.g. lineHash['Hello\n'] == 4
+
+        // '\x00' is a valid character, but various debuggers don't like it.
+        // So we'll insert a junk entry to avoid generating a null character.
+        lineArray[0] = "";
+
+        // Allocate 2/3rds of the space for text1, the rest for text2.
+        const chars1 = this.diff_linesToCharsMunge_(text1, lineArray, lineHash, 40000);
+        const chars2 = this.diff_linesToCharsMunge_(text2, lineArray, lineHash, 65535);
+        return { chars1, chars2, lineArray };
+    }
+
+    /**
+     * Split a text into an array of strings. Reduce the texts to a string of
+     * hashes where each Unicode character represents one line.
+     * Modifies linearray and linehash through being a closure.
+     *
+     * @private
+     * @param {string} text String to encode.
+     * @return {string} Encoded string.
+     * @param {string[]} lineArray The array of unique strings.
+     * @param {Record<string, number>} lineHash The line-hash pairs.
+     * @param {number} maxLines
+     */
+    private diff_linesToCharsMunge_(
+        text: string,
+        lineArray: string[],
+        lineHash: Record<string, number>,
+        maxLines: number
+    )
+    {
+        let chars = "";
+        // Walk the text, pulling out a substring for each line.
+        // text.split('\n') would would temporarily double our memory footprint.
+        // Modifying text would create many large strings to garbage collect.
+        let lineStart = 0;
+        let lineEnd = -1;
+        // Keeping our own length variable is faster than looking it up.
+        let lineArrayLength = lineArray.length;
+        while (lineEnd < text.length - 1)
+        {
+            lineEnd = text.indexOf("\n", lineStart);
+            if (lineEnd === -1)
+            {
+                lineEnd = text.length - 1;
+            }
+            let line = text.substring(lineStart, lineEnd + 1);
+
+            if (
+                lineHash.hasOwnProperty
+                    ? lineHash.hasOwnProperty(line)
+                    : (lineHash[line] !== undefined)
+            )
+            {
+                chars += String.fromCharCode(lineHash[line]);
+            }
+            else
+            {
+                if (lineArrayLength === maxLines)
+                {
+                    // Bail out at 65535 because
+                    // String.fromCharCode(65536) == String.fromCharCode(0)
+                    line = text.substring(lineStart);
+                    lineEnd = text.length;
+                }
+                chars += String.fromCharCode(lineArrayLength);
+                lineHash[line] = lineArrayLength;
+                lineArray[lineArrayLength++] = line;
+            }
+            lineStart = lineEnd + 1;
+        }
+        return chars;
+    }
 }
