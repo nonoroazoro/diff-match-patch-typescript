0f7f89941b55fd1bd4fcc1d18edac17772a13f63
nonoroazoro <xiaochao.k@gmail.com>
2019-02-28 10:53:20 +0800

    Add diff_bisect_

--- src/DiffMatchPatch.ts
+++ src/DiffMatchPatch.ts
@@ -83,7 +83,8 @@ export class DiffMatchPatch
             if (this.Diff_Timeout <= 0)
             {
                 opt_deadline = Number.MAX_VALUE;
-            } else
+            }
+            else
             {
                 opt_deadline = Date.now() + this.Diff_Timeout * 1000;
             }
@@ -296,5 +297,164 @@ export class DiffMatchPatch
         diffs.pop();
 
         return diffs;
+    }
+
+    /**
+     * Find the 'middle snake' of a diff, split the problem in two
+     * and return the recursively constructed diff.
+     * See Myers 1986 paper: An O(ND) Difference Algorithm and Its Variations.
+     *
+     * @private
+     * @param {string} text1 Old string to be diffed.
+     * @param {string} text2 New string to be diffed.
+     * @param {number} deadline Time at which to bail if not yet complete.
+     * @returns {Diff[]} Array of diff tuples.
+     */
+    private diff_bisect_(text1: string, text2: string, deadline: number)
+    {
+        // Cache the text lengths to prevent multiple calls.
+        const text1_length = text1.length;
+        const text2_length = text2.length;
+        const max_d = Math.ceil((text1_length + text2_length) / 2);
+        const v_offset = max_d;
+        const v_length = 2 * max_d;
+        const v1 = new Array(v_length);
+        const v2 = new Array(v_length);
+        // Setting all elements to -1 is faster in Chrome & Firefox than mixing
+        // integers and undefined.
+        for (let x = 0; x < v_length; x++)
+        {
+            v1[x] = -1;
+            v2[x] = -1;
+        }
+        v1[v_offset + 1] = 0;
+        v2[v_offset + 1] = 0;
+        const delta = text1_length - text2_length;
+        // If the total number of characters is odd, then the front path will collide
+        // with the reverse path.
+        const front = (delta % 2 !== 0);
+        // Offsets for start and end of k loop.
+        // Prevents mapping of space beyond the grid.
+        let k1start = 0;
+        let k1end = 0;
+        let k2start = 0;
+        let k2end = 0;
+        for (let d = 0; d < max_d; d++)
+        {
+            // Bail out if deadline is reached.
+            if (Date.now() > deadline)
+            {
+                break;
+            }
+
+            // Walk the front path one step.
+            for (let k1 = -d + k1start; k1 <= d - k1end; k1 += 2)
+            {
+                const k1_offset = v_offset + k1;
+                let x1;
+                if (k1 === -d || (k1 !== d && v1[k1_offset - 1] < v1[k1_offset + 1]))
+                {
+                    x1 = v1[k1_offset + 1];
+                }
+                else
+                {
+                    x1 = v1[k1_offset - 1] + 1;
+                }
+
+                let y1 = x1 - k1;
+                while (
+                    x1 < text1_length
+                    && y1 < text2_length
+                    && text1.charAt(x1) === text2.charAt(y1)
+                )
+                {
+                    x1++;
+                    y1++;
+                }
+                v1[k1_offset] = x1;
+                if (x1 > text1_length)
+                {
+                    // Ran off the right of the graph.
+                    k1end += 2;
+                }
+                else if (y1 > text2_length)
+                {
+                    // Ran off the bottom of the graph.
+                    k1start += 2;
+                }
+                else if (front)
+                {
+                    const k2_offset = v_offset + delta - k1;
+                    if (k2_offset >= 0 && k2_offset < v_length && v2[k2_offset] !== -1)
+                    {
+                        // Mirror x2 onto top-left coordinate system.
+                        const x2 = text1_length - v2[k2_offset];
+                        if (x1 >= x2)
+                        {
+                            // Overlap detected.
+                            return this.diff_bisectSplit_(text1, text2, x1, y1, deadline);
+                        }
+                    }
+                }
+            }
+
+            // Walk the reverse path one step.
+            for (let k2 = -d + k2start; k2 <= d - k2end; k2 += 2)
+            {
+                const k2_offset = v_offset + k2;
+                let x2;
+                if (k2 === -d || (k2 !== d && v2[k2_offset - 1] < v2[k2_offset + 1]))
+                {
+                    x2 = v2[k2_offset + 1];
+                }
+                else
+                {
+                    x2 = v2[k2_offset - 1] + 1;
+                }
+                let y2 = x2 - k2;
+                while (
+                    x2 < text1_length
+                    && y2 < text2_length
+                    && text1.charAt(text1_length - x2 - 1) === text2.charAt(text2_length - y2 - 1)
+                )
+                {
+                    x2++;
+                    y2++;
+                }
+                v2[k2_offset] = x2;
+                if (x2 > text1_length)
+                {
+                    // Ran off the left of the graph.
+                    k2end += 2;
+                }
+                else if (y2 > text2_length)
+                {
+                    // Ran off the top of the graph.
+                    k2start += 2;
+                }
+                else if (!front)
+                {
+                    const k1_offset = v_offset + delta - k2;
+                    if (k1_offset >= 0 && k1_offset < v_length && v1[k1_offset] !== -1)
+                    {
+                        const x1 = v1[k1_offset];
+                        const y1 = v_offset + x1 - k1_offset;
+                        // Mirror x2 onto top-left coordinate system.
+                        x2 = text1_length - x2;
+                        if (x1 >= x2)
+                        {
+                            // Overlap detected.
+                            return this.diff_bisectSplit_(text1, text2, x1, y1, deadline);
+                        }
+                    }
+                }
+            }
+        }
+        // Diff took too long and hit the deadline or
+        // number of diffs equals number of characters, no commonality at all.
+        return [
+            [DiffOperation.DIFF_DELETE, text1],
+            [DiffOperation.DIFF_INSERT, text2]
+        ];
     }
 }
