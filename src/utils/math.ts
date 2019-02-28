495a098a1526416b58b3e3d0d4e3494d545c7225
nonoroazoro <xiaochao.k@gmail.com>
2019-02-28 15:10:35 +0800

    Add math util

---
+++ src/utils/math.ts
@@ -0,0 +1,15 @@
+/**
+ * Returns the smaller number.
+ */
+export function min(a: number, b: number)
+{
+    return a <= b ? a : b;
+}
+
+/**
+ * Returns the larger number.
+ */
+export function max(a: number, b: number)
+{
+    return a >= b ? a : b;
+}
