62114306845580bf02fa51f3c36f584ef2e4fbcc
nonoroazoro <xiaochao.k@gmail.com>
2019-02-28 15:24:03 +0800

    Add diff_cleanupSemanticLossless and diff_cleanupSemanticScore_

--- src/DiffMatchPatch.ts
+++ src/DiffMatchPatch.ts
@@ -1,3 +1,11 @@
+import
+{
+    blanklineEndRegex_,
+    blanklineStartRegex_,
+    linebreakRegex_,
+    nonAlphaNumericRegex_,
+    whitespaceRegex_
+} from "./constants";
 import { Diff, DiffOperation, HalfMatchArray } from "./types";
 import * as math from "./utils/math";
 
@@ -354,6 +362,88 @@ export class DiffMatchPatch
                     }
                 }
                 pointer++;
+            }
+            pointer++;
+        }
+    }
+
+    /**
+     * Look for single edits surrounded on both sides by equalities
+     * which can be shifted sideways to align the edit to a word boundary.
+     * e.g: The c<ins>at c</ins>ame. -> The <ins>cat </ins>came.
+     *
+     * @param {Diff[]} diffs Array of diff tuples.
+     */
+    public diff_cleanupSemanticLossless(diffs: Diff[]): void
+    {
+        let pointer = 1;
+        // Intentionally ignore the first and last element (don't need checking).
+        while (pointer < diffs.length - 1)
+        {
+            if (diffs[pointer - 1][0] === DiffOperation.DIFF_EQUAL &&
+                diffs[pointer + 1][0] === DiffOperation.DIFF_EQUAL)
+            {
+                // This is a single edit surrounded by equalities.
+                let equality1 = diffs[pointer - 1][1];
+                let edit = diffs[pointer][1];
+                let equality2 = diffs[pointer + 1][1];
+
+                // First, shift the edit as far left as possible.
+                const commonOffset = this.diff_commonSuffix(equality1, edit);
+                if (commonOffset)
+                {
+                    const commonString = edit.substring(edit.length - commonOffset);
+                    equality1 = equality1.substring(0, equality1.length - commonOffset);
+                    edit = commonString + edit.substring(0, edit.length - commonOffset);
+                    equality2 = commonString + equality2;
+                }
+
+                // Second, step character by character right, looking for the best fit.
+                let bestEquality1 = equality1;
+                let bestEdit = edit;
+                let bestEquality2 = equality2;
+                let bestScore = this.diff_cleanupSemanticScore_(equality1, edit)
+                    + this.diff_cleanupSemanticScore_(edit, equality2);
+                while (edit.charAt(0) === equality2.charAt(0))
+                {
+                    equality1 += edit.charAt(0);
+                    edit = edit.substring(1) + equality2.charAt(0);
+                    equality2 = equality2.substring(1);
+                    const score = this.diff_cleanupSemanticScore_(equality1, edit)
+                        + this.diff_cleanupSemanticScore_(edit, equality2);
+                    // The >= encourages trailing rather than leading whitespace on edits.
+                    if (score >= bestScore)
+                    {
+                        bestScore = score;
+                        bestEquality1 = equality1;
+                        bestEdit = edit;
+                        bestEquality2 = equality2;
+                    }
+                }
+
+                if (diffs[pointer - 1][1] !== bestEquality1)
+                {
+                    // We have an improvement, save it back to the diff.
+                    if (bestEquality1)
+                    {
+                        diffs[pointer - 1][1] = bestEquality1;
+                    }
+                    else
+                    {
+                        diffs.splice(pointer - 1, 1);
+                        pointer--;
+                    }
+                    diffs[pointer][1] = bestEdit;
+                    if (bestEquality2)
+                    {
+                        diffs[pointer + 1][1] = bestEquality2;
+                    }
+                    else
+                    {
+                        diffs.splice(pointer + 1, 1);
+                        pointer--;
+                    }
+                }
             }
             pointer++;
         }
@@ -956,7 +1046,7 @@ export class DiffMatchPatch
      * @param {string} longtext Longer string.
      * @param {string} shorttext Shorter string.
      * @param {number} i Start index of quarter length substring within longtext.
-     * @return {(HalfMatchArray | null)} Five element Array, containing the prefix of
+     * @returns {(HalfMatchArray | null)} Five element Array, containing the prefix of
      * longtext, the suffix of longtext, the prefix of shorttext, the suffix
      * of shorttext and the common middle.  Or null if there was no match.
      */
@@ -1007,5 +1097,68 @@ export class DiffMatchPatch
             ];
         }
         return null;
+    }
+
+    /**
+     * Given two strings, compute a score representing whether the internal
+     * boundary falls on logical boundaries.
+     * Scores range from 6 (best) to 0 (worst).
+     * Closure, but does not reference any external variables.
+     *
+     * @private
+     * @param {string} one First string.
+     * @param {string} two Second string.
+     * @returns {number} The score.
+     */
+    private diff_cleanupSemanticScore_(one: string, two: string): number
+    {
+        if (!one || !two)
+        {
+            // Edges are the best.
+            return 6;
+        }
+
+        // Each port of this function behaves slightly differently due to
+        // subtle differences in each language's definition of things like
+        // 'whitespace'.  Since this function's purpose is largely cosmetic,
+        // the choice has been made to use each language's native features
+        // rather than force total conformity.
+        const char1 = one.charAt(one.length - 1);
+        const char2 = two.charAt(0);
+        const nonAlphaNumeric1 = char1.match(nonAlphaNumericRegex_);
+        const nonAlphaNumeric2 = char2.match(nonAlphaNumericRegex_);
+        const whitespace1 = nonAlphaNumeric1 && char1.match(whitespaceRegex_);
+        const whitespace2 = nonAlphaNumeric2 && char2.match(whitespaceRegex_);
+        const lineBreak1 = whitespace1 && char1.match(linebreakRegex_);
+        const lineBreak2 = whitespace2 && char2.match(linebreakRegex_);
+        const blankLine1 = lineBreak1 && one.match(blanklineEndRegex_);
+        const blankLine2 = lineBreak2 && two.match(blanklineStartRegex_);
+
+        if (blankLine1 || blankLine2)
+        {
+            // Five points for blank lines.
+            return 5;
+        }
+        else if (lineBreak1 || lineBreak2)
+        {
+            // Four points for line breaks.
+            return 4;
+        }
+        else if (nonAlphaNumeric1 && !whitespace1 && whitespace2)
+        {
+            // Three points for end of sentences.
+            return 3;
+        }
+        else if (whitespace1 || whitespace2)
+        {
+            // Two points for whitespace.
+            return 2;
+        }
+        else if (nonAlphaNumeric1 || nonAlphaNumeric2)
+        {
+            // One point for non-alphanumeric.
+            return 1;
+        }
+        return 0;
     }
 }
