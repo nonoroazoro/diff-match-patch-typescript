234c4ec846624ef187fb55f3f4b7e0c21bf0a54d
nonoroazoro <xiaochao.k@gmail.com>
2019-03-01 15:30:38 +0800

    Add testing to PATCH - Make

--- src/__test__/core/DiffMatchPatch.test.ts
+++ src/__test__/core/DiffMatchPatch.test.ts
@@ -937,5 +937,92 @@ describe("diff-match-patch-ts - core/DiffMatchPatch", () =>
     p = dmp.patch_fromText(strp);
     expect(strp).toEqual(dmp.patch_toText(p));
   });
+
+  it("PATCH - Add Context", () =>
+  {
+    dmp.Patch_Margin = 4;
+    let p = dmp.patch_fromText("@@ -21,4 +21,10 @@\n-jump\n+somersault\n")[0];
+    dmp["patch_addContext_"](p, "The quick brown fox jumps over the lazy dog.");
+    expect("@@ -17,12 +17,18 @@\n fox \n-jump\n+somersault\n s ov\n").toEqual(p.toString());
+
+    // Same, but not enough trailing context.
+    p = dmp.patch_fromText("@@ -21,4 +21,10 @@\n-jump\n+somersault\n")[0];
+    dmp["patch_addContext_"](p, "The quick brown fox jumps.");
+    expect("@@ -17,10 +17,16 @@\n fox \n-jump\n+somersault\n s.\n").toEqual(p.toString());
+
+    // Same, but not enough leading context.
+    p = dmp.patch_fromText("@@ -3 +3,2 @@\n-e\n+at\n")[0];
+    dmp["patch_addContext_"](p, "The quick brown fox jumps.");
+    expect("@@ -1,7 +1,8 @@\n Th\n-e\n+at\n  qui\n").toEqual(p.toString());
+
+    // Same, but with ambiguity.
+    p = dmp.patch_fromText("@@ -3 +3,2 @@\n-e\n+at\n")[0];
+    dmp["patch_addContext_"](p, "The quick brown fox jumps.  The quick brown fox crashes.");
+    expect("@@ -1,27 +1,28 @@\n Th\n-e\n+at\n  quick brown fox jumps. \n").toEqual(p.toString());
+  });
+
+  it("PATCH - Make", () =>
+  {
+    // Null case.
+    let patches = dmp.patch_make("", "");
+    expect("").toEqual(dmp.patch_toText(patches));
+
+    let text1 = "The quick brown fox jumps over the lazy dog.";
+    let text2 = "That quick brown fox jumped over a lazy dog.";
+    // Text2+Text1 inputs.
+    let expectedPatch = "@@ -1,8 +1,7 @@\n Th\n-at\n+e\n  qui\n@@ -21,17 +21,18 @@\n jump\n-ed\n+s\n  over \n-a\n+the\n  laz\n";
+    // The second patch must be "-21,17 +21,18", not "-22,17 +21,18" due to rolling context.
+    patches = dmp.patch_make(text2, text1);
+    expect(expectedPatch).toEqual(dmp.patch_toText(patches));
+
+    // Text1+Text2 inputs.
+    expectedPatch = "@@ -1,11 +1,12 @@\n Th\n-e\n+at\n  quick b\n@@ -22,18 +22,17 @@\n jump\n-s\n+ed\n  over \n-the\n+a\n  laz\n";
+    patches = dmp.patch_make(text1, text2);
+    expect(expectedPatch).toEqual(dmp.patch_toText(patches));
+
+    // Diff input.
+    let diffs = dmp.diff_main(text1, text2, false);
+    patches = dmp.patch_make(diffs);
+    expect(expectedPatch).toEqual(dmp.patch_toText(patches));
+
+    // Text1+Diff inputs.
+    patches = dmp.patch_make(text1, diffs);
+    expect(expectedPatch).toEqual(dmp.patch_toText(patches));
+
+    // Text1+Text2+Diff inputs (deprecated).
+    patches = dmp.patch_make(text1, text2, diffs);
+    expect(expectedPatch).toEqual(dmp.patch_toText(patches));
+
+    // Character encoding.
+    patches = dmp.patch_make("`1234567890-=[]\\;',./", '~!@#$%^&*()_+{}|:"<>?');
+    expect("@@ -1,21 +1,21 @@\n-%601234567890-=%5B%5D%5C;',./\n+~!@#$%25%5E&*()_+%7B%7D%7C:%22%3C%3E?\n").toEqual(dmp.patch_toText(patches));
+
+    // Character decoding.
+    diffs = [[DiffOperation.DIFF_DELETE, "`1234567890-=[]\\;',./"], [DiffOperation.DIFF_INSERT, '~!@#$%^&*()_+{}|:"<>?']];
+    expect(dmp.patch_fromText("@@ -1,21 +1,21 @@\n-%601234567890-=%5B%5D%5C;',./\n+~!@#$%25%5E&*()_+%7B%7D%7C:%22%3C%3E?\n")[0].diffs).toEqual(diffs);
+
+    // Long string with repeats.
+    text1 = "";
+    for (let x = 0; x < 100; x++)
+    {
+      text1 += "abcdef";
+    }
+    text2 = text1 + "123";
+    expectedPatch = "@@ -573,28 +573,31 @@\n cdefabcdefabcdefabcdefabcdef\n+123\n";
+    patches = dmp.patch_make(text1, text2);
+    expect(expectedPatch).toEqual(dmp.patch_toText(patches));
+
+    // Test null inputs.
+    try
+    {
+      dmp.patch_make(null as any);
+      fail("Should generates error of null inputs");
+    }
+    catch (e)
+    {
+      // Exception expected.
+      expect(e.message).toEqual("Unknown call format to patch_make");
+    }
+  });
   //#endregion PATCH TEST FUNCTIONS
 });
