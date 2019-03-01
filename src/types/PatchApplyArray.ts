1e96f104413594d361841c0f5adffdced1256d35
nonoroazoro <xiaochao.k@gmail.com>
2019-03-01 16:09:02 +0800

    Add PatchApplyArray

---
+++ src/types/PatchApplyArray.ts
@@ -0,0 +1,9 @@
+/**
+ * Represents the results of the `patch_apply` method.
+ *
+ * Two element Array, containing the
+ * new text and an array of boolean values.
+ *
+ * Ported by [xiaochao.k@gmail.com](https://github.com/nonoroazoro)
+ */
+export type PatchApplyArray = [string, boolean[]];
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export * from "./Diff";
 export * from "./DiffOperation";
 export * from "./HalfMatchArray";
+export * from "./PatchApplyArray";
